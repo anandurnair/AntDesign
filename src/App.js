f352698e3603068b414266d1b1533663ffd83351
anandurnair <anandurpallam@gmail.com>
2024-10-14 15:51:15 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import { DatePicker, Space,Spin } from 'antd';
-import { Color } from 'antd/es/color-picker';
-import { LoadingOutlined } from '@ant-design/icons'; // Import the loading icon
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { DatePicker, Space, Spin } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+
+import { Color } from "antd/es/color-picker";
+import { LoadingOutlined } from "@ant-design/icons"; // Import the loading icon
 
 const { Header, Content, Footer } = Layout;
 const items = new Array(15).fill(null).map((_, index) => ({
@@ -15,26 +17,34 @@ const App = () => {
   };
   const spinnerIcon = (
     <LoadingOutlined
-      style={{ fontSize: 24, color: 'red' }} // Customize the size and color here
+      style={{ fontSize: 24, color: "red" }} // Customize the size and color here
       spin
     />
   );
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <Layout>
       <Header
         style={{
-          display: 'flex',
-          alignItems: 'center',
+          display: "flex",
+          alignItems: "center",
         }}
       >
         <div className="demo-logo" />
         <Menu
           theme="dark"
           mode="horizontal"
-          defaultSelectedKeys={['2']}
+          defaultSelectedKeys={["2"]}
           items={items}
           style={{
             flex: 1,
@@ -44,12 +54,12 @@ const App = () => {
       </Header>
       <Content
         style={{
-          padding: '0 48px',
+          padding: "0 48px",
         }}
       >
         <Breadcrumb
           style={{
-            margin: '16px 0',
+            margin: "16px 0",
           }}
         >
           <Breadcrumb.Item>Home</Breadcrumb.Item>
@@ -59,24 +69,91 @@ const App = () => {
         <div
           style={{
             background: colorBgContainer,
-            height:800,
+            height: 800,
             padding: 24,
             borderRadius: borderRadiusLG,
           }}
         >
           <Space direction="vertical">
-    <DatePicker onChange={onChange} />
-    <Spin indicator={spinnerIcon} />
-  </Space>
+            <DatePicker onChange={onChange} />
+            <Spin indicator={spinnerIcon} />
+            <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+          </Space>
         </div>
+       
       </Content>
-      <Footer
-        style={{
-          textAlign: 'center',
-        }}
-      >
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
+      
+    
+
+
+
+
+
     </Layout>
   );
 };
